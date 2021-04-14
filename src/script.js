461b1812c6f337454ae944b41922b2d17e071d0f
Cory Thomas <cory.a.thomas1992@gmail.com>
2021-04-14 12:46:05 -0400

    added textures and lights. The texture was a real challenge for me, but I got about 95% of it complete, ran into an issue with the cube sides seperating.

--- src/script.js
+++ src/script.js
@@ -1,6 +1,28 @@
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'dat.gui';
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI();
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+
+const colorTexture = textureLoader.load('/textures/metal_grill/basecolor.jpg');
+const ambientOcclusionTexture = textureLoader.load(
+  '/textures/metal_grill/ambientOcclusion.jpg'
+);
+const heightTexture = textureLoader.load('/textures/metal_grill/height.png');
+const normalTexture = textureLoader.load('/textures/metal_grill/normal.jpg');
+const metallicTexture = textureLoader.load('/textures/metal_grill/normal.jpg');
+const roughnessTexture = textureLoader.load(
+  '/textures/metal_grill/roughness.jpg'
+);
 
 /**
  * Base
@@ -14,11 +36,47 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
+const material = new THREE.MeshStandardMaterial();
+// material.metalness = 0;
+// material.roughness = 1;
+material.map = colorTexture;
+material.aoMap = ambientOcclusionTexture;
+material.aoMapIntensity = 3;
+material.displacementMap = heightTexture;
+material.displacementScale = 0.08;
+material.normalMap = normalTexture;
+material.normalScale.set(1, 1);
+material.transparent = true;
+material.metalnessMap = metallicTexture;
+material.roughnessMap = roughnessTexture;
+
+gui.add(material, 'metalness').min(0).max(1).step(0.0001);
+gui.add(material, 'roughness').min(0).max(10).step(0.0001);
+gui.add(material, 'aoMapIntensity').min(0).max(10).step(0.0001);
+gui.add(material, 'displacementScale').min(0).max(1).step(0.0001);
+
 const box = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+  new THREE.BoxGeometry(5, 5, 5, 160, 160, 160),
+  material
 );
+box.geometry.setAttribute(
+  'uv2',
+  new THREE.BufferAttribute(box.geometry.attributes.uv.array, 2)
+);
+
 scene.add(box);
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 1.5);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 6;
+scene.add(pointLight);
 
 // Sizes
 const sizes = {
@@ -52,7 +110,7 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.x = 1;
 camera.position.y = 1;
-camera.position.z = 3;
+camera.position.z = 7;
 scene.add(camera);
 
 // Controls
@@ -66,6 +124,9 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+/**
+ * Animate
+ */
 const clock = new THREE.Clock();
 
 const tick = () => {
